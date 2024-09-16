9ffef280c06a358bf55887854c26e61352e91d7f
alaynab2105 <s20126793@students.duvalschools.org>
2024-09-16 13:09:07 -0400

    projniect

--- 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
+++ 01_gaming_exercises/00_awarding_extra_lives/awarding_extra_lives.py
@@ -2,8 +2,13 @@ lives = 3
 playerScore = int(input("put your score in\n"))
 
 if playerScore <= 10000:
+    lives -= 1
     print(f"my boyfriend scored {playerScore}points and lost a life\n")
 elif playerScore < 100001:
-     print(f"my boyfriend scored {playerScore}points and got a extra life\n")
-else:
+    lives += 1
+    print(f"my boyfriend scored {playerScore}points and got a extra life\n")
+elif playerScore > 100000:
+    lives += 2
     print(f"my boyfriend scored {playerScore}points and earned two extra lives\n")   
+
+print(f"you have {lives} lives remaining.\n")
