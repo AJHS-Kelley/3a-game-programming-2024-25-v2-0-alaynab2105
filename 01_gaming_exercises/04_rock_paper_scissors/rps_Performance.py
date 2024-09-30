1242a59575b4749ff5606b723718cf2b251ab396
alaynab2105 <s20126793@students.duvalschools.org>
2024-09-30 12:39:12 -0400

    dfda
    dfafdfe

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rps_Performance.py
@@ -0,0 +1,133 @@
+# Rock, Paper, Scissors by Alayna Brewer, v0.0
+
+# MODULE IMPORTS
+import random, time
+
+# DATA STUCTURES -- PLAYER
+playerScore = 0
+playerChoice = None
+numDraws = 0
+# DATA STUCTURES -- CPU
+cpuScore = 0
+cpuChoice = None
+
+
+ # MAIN GAME LOOP 
+loopCount= 0
+loopsReq = input("How many loops do you want\nEnter an integer, no commas, and press ENTER ")
+# req is the universal abbreviation in computer programming for REQUEST. reqs= REQUEST
+rpsTimeStart = time.time() # returns the number of seconds since Jan. 01, 1970 @ 12:00AM
+while playerScore < 5 and cpuScore < 5:
+    
+       # let cpu select choice at random.
+        cpuChoice = random.randint(0,2) # randomly select 0,1, or 2.
+        if cpuChoice == 0:
+            cpuChoice= "rock"
+elif cpuChoice == 1:
+        cpuChoice ="paper"
+elif cpuChoice == 2:
+        cpuChoice = "scissors"
+else:
+ print("unable to determine CPU choice.\nPlease restart.\n")
+exit()
+print(f"CPU Choice:{cpuChoice}")
+
+    # let playerChoice select choice at random.
+playerChoice = random.randint(0,2) # randomly select 0,1, or 2.
+if playerChoice == 0:
+    playerChoice= "rock"
+elif playerChoice == 1:
+    playerChoice ="paper"
+elif playerChoice == 2:
+    playerChoice = "scissors"
+else:
+ print("unable to determine CPU choice.\nPlease restart.\n")
+exit()
+print(f"CPU Choice:{cpuChoice}")
+        
+        
+        
+        
+        
+        # compare player choice to cpu choice 
+if playerChoice == "rock" and cpuChoice =="paper":
+        print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+        print("The CPU wins a point.\n")
+        cpuScore+=1
+        # CPU WINS
+elif playerChoice =="rock" and cpuChoice == "scissors":
+        # PLAYER WINS 
+            print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+            print("You win a point.\n")
+            playerScore += 1
+elif playerChoice == "rock" and cpuChoice == "rock":
+            # DRAW
+            print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+            print("It's a draw!\n")
+elif playerChoice =="scissors" and cpuChoice == "rock":
+            # CPU WINS 
+        print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+        print("The CPU wins a point.\n") 
+        cpuScore+=1
+elif playerChoice == "scissors" and cpuChoice == "paper": 
+            # PLAYER WINS 
+            print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n") 
+            print("You win a point.\n")
+            playerScore += 1 
+elif playerChoice == "scissors" and cpuChoice == "scissors":
+            # DRAW
+            print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+            print("It's a draw!\n")
+elif playerChoice == "paper" and cpuChoice == "rock":
+            # PLAYER WINS 
+            print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+            print("You win a point.\n")
+            playerScore += 1 
+elif playerChoice == "paper" and cpuChoice == "paper": 
+            # DRAW
+            print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+            print("It's a draw!\n") 
+elif playerChoice == "paper" and cpuChoice == "scissors":
+            # CPU WINS
+            print(f"The CPU chose {cpuChoice} and you chose {playerChoice}.\n")
+            print("The CPU wins a point.\n")
+            cpuScore+=1 
+else:
+            print("Unable to determine a winner. Please restart.\n")
+            exit()
+
+
+print(f"Your Final Score: {playerScore}CPU Final Score: {cpuScore}\n")
+if  playerScore > cpuScore:
+    print(f"Congratulations {playerName}, a winner is you!\n")
+elif cpuScore > playerScore:
+    print(f"The CPU wins. You are a disappointment to all.\n")
+else:
+    print("Unable to determine a winner.\nPlease restart.\n")
+    exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
